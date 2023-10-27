80f1301c22beb2da24f7eff313c1b9bdcca7c1ad
FrodinLP <ingo.neubert92@gmx.de>
2023-10-27 10:34:48 +0200

    Wertebereich ermitteln aktuell

--- wertebereich.py
+++ wertebereich.py
@@ -6,9 +6,9 @@ y = 15
 
 # Funktion überprüft, ob die Werte x und y in einem gültigen Bereich liegen
 def is_valid_range(x, y):
-    if 0 < x < 100 and 10 < y < 20:  # Prüfe, ob x zwischen 0 und 100 und y zwischen 10 und 20 liegt.
-        print("VALID RANGE")  # Wenn die Bedingung erfüllt ist, gebe "VALID RANGE" aus.
-    else: print("UNVALID RANGE")
+    if 0 < x < 100 and 10 < y < 20:  # Prüfe, ob x zwischen 0 und 100 und y zwischen 10 und 20 liegt
+        print("gültiger Bereich")  # Wenn Bedingung erfüllt ist, gebe "gültiger Bereich" aus
+    else: print("ungültiger Bereich") # Wenn Bedingung nicht erfüllt ist, gebe "ungültiger Bereich" aus
 
 # Funktion abrufen
 is_valid_range(x, y)
